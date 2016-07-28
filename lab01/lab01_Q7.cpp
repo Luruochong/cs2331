f57aa8d22f43c267fe3f13edf8d31b07910486e9
Ryan lu <ruochonglu@gmail.com>
2016-07-28 19:48:33 +0800

    Lab 01 Q7.

---
+++ lab01/lab01_Q7.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+void main(){
+    int n;
+    cout << "Input the number of credit units of this course: ";
+    cin >> n;
+    cout << "In the semester, you need to study approximately";
+    cout << 40 * n;
+    cout << " to ";
+    cout << 50 * n;
+    cout << " hours for this course." << endl;
+
+
+
+}
+
+
+
+
