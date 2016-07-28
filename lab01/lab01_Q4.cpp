9d1e4a4170437bdd709deea954acd7228ad597b7
Ryan lu <ruochonglu@gmail.com>
2016-07-28 19:39:13 +0800

    lab 01 Q4.

---
+++ lab01/lab01_Q4.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+
+void main(){
+
+    char choice123;
+    char choicewh;
+    cout << "input your choice('1' , '2' or '3'):";
+    cin >> choice123;
+
+    cout << "input your choice('w' or 'h'):";
+    cin >> choicewh;
+
+    if (choice123=='1'){
+
+	if (choicewh=='w'){
+	    cout << "Welcome,world";
+	    cout << endl;
+	}
+
+	if (choicewh=='h'){
+	    cout << "Hello,world";
+	    cout << endl;
+	}
+
+    }
+
+    if (choice123=='2'){
+
+	if (choicewh=='w'){
+	    cout << "Welcome,world";
+	    cout << endl;
+	}
+
+	if (choicewh=='h'){
+	    cout << "Hello,world";
+	    cout << endl;
+	}
+
+    }
+
+    if (choice123=='3'){
+
+	if (choicewh=='w'){
+	    cout << "Welcome,world";
+	    cout << endl;
+	}
+
+	if (choicewh=='h'){
+	    cout << "Hello,world";
+	    cout << endl;
+	}
+
+    }
+}
+
+
+
+
