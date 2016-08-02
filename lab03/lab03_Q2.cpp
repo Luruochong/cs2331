17cd07d6d6920efc5cff3f2d3d189c1168e99053
Ryan lu <ruochonglu@gmail.com>
2016-08-02 12:12:52 +0800

    change to use function.

--- lab03/lab03_Q2.cpp
+++ lab03/lab03_Q2.cpp
@@ -1,17 +1,23 @@
 #include <iostream>
 using namespace std;
 
+int isLeapYear(int year){
+    if (year % 4 == 0){
+        cout << year << " is a leap year." << endl;
+    }
+    else 
+        cout << year << " is a common year." << endl;
+
+    return 0;
+}
+
 int main(){
     int year;
     char conti;
     while (conti != 'n'){
         cout << "Input a year: ";
         cin >> year;
-        if (year % 4 == 0){
-            cout << year << " is a leap year." << endl;
-        }
-        else 
-            cout << year << " is a common year." << endl;
+        isLeapYear(year);
         cout << "Continue? Your choice[y/n]: ";
         cin >> conti;
     }
